62f4f4116acb344ffe1c4fab33ceb08c40a2f31e
RabeyaMuna <rabeyakhatun.muna@mail.concordia.ca>
2026-07-08 11:59:01 -0400

    107

--- framework/py/flwr/server/strategy/aggregate.py
+++ framework/py/flwr/server/strategy/aggregate.py
@@ -53,7 +53,7 @@ def aggregate_inplace(results: list[tuple[ClientProxy, FitRes]]) -> NDArrays:
     ]
 
     def _try_inplace(
-        x: NDArray, y: Union[NDArray, np.float64], np_binary_op: np.ufunc
+        x: NDArray, y: Union[NDArray, float, np.float64], np_binary_op: np.ufunc
     ) -> NDArray:
         y_arr = np.asarray(y)
         return cast(
