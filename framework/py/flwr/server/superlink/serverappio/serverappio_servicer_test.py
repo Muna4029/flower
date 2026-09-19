c65f7d16eb46e9871dad14341d6b5f81a2421094
rabeyakhatunmuna <rabeyakhatunmuna@Rabeyas-MacBook-Pro.local>
2026-09-19 14:12:55 -0400

    Muna4029__diff__id_109

--- framework/py/flwr/server/superlink/serverappio/serverappio_servicer_test.py
+++ framework/py/flwr/server/superlink/serverappio/serverappio_servicer_test.py
@@ -14,7 +14,6 @@
 # ==============================================================================
 """ServerAppIoServicer tests."""
 
-
 import tempfile
 import unittest
 from typing import Optional
@@ -331,9 +330,9 @@ class TestServerAppIoServicer(unittest.TestCase):  # pylint: disable=R0902
 
     @parameterized.expand(
         [
-            # The normal case: the message is recognized by both `LinkState` and `ObjectStore`
+            # Normal case: message recognized by both `LinkState` and `ObjectStore`
             (True,),
-            # The failure case: the message is found in `LinkState` but not in `ObjectStore`
+            # Failure case: message found in `LinkState` but not in `ObjectStore`
             (False,),
         ]
     )  # type: ignore
