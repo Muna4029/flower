96d06e076f51172104075125eb2cfe50a018f31c
rabeyakhatunmuna <rabeyakhatunmuna@Rabeyas-MacBook-Pro.local>
2026-09-25 21:32:07 -0400

    Muna4029__diff__id_111

--- framework/py/flwr/common/record/arrayrecord.py
+++ framework/py/flwr/common/record/arrayrecord.py
@@ -214,7 +214,7 @@ class ArrayRecord(TypedDict[str, Array], InflatableObject):
             # Type check the input
             # pylint: disable-next=not-an-iterable
             if isinstance(arg, list) and all(isinstance(v, np.ndarray) for v in arg):
-                numpy_ndarrays = cast(list[NDArray], arg)
+                numpy_ndarrays = arg
                 converted = self.from_numpy_ndarrays(
                     numpy_ndarrays, keep_input=keep_input
                 )
